c45e98abd6fdce9489ddfd5a8dc354848e793de7
medreres <90519417+medreres@users.noreply.github.com>
2023-02-16 20:25:42 +0200

    main pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router'
 import Footer from './layout/Footer'
 import Navbar from './layout/Navbar'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Profile from './pages/Profile'
+import Project from './pages/Project'
 
 function App() {
   return (
@@ -9,9 +13,16 @@ function App() {
       <Navbar />
       <Routes>
         {/* Home Page */}
-        <Route path='/' element={<div>Home Page</div>} />
+        <Route path='/' element={<Home />} />
 
-        
+        {/* Project Page */}
+        <Route path='/projects/:projectId' element={<Project />} />
+
+        {/* Profile Page */}
+        <Route path='/profile/:userId' element={<Profile />} />
+
+        {/* Not Found Page */}
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </>
