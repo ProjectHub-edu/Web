b0885ff189afddc213b8e652cebe90401af88073
medreres <90519417+medreres@users.noreply.github.com>
2023-02-20 11:55:08 +0200

    create project page with mui

--- src/pages/CreateProject.tsx
+++ src/pages/CreateProject.tsx
@@ -1,20 +1,68 @@
 import React from 'react'
-import '../features/projects/styles/CreateProject.scss'
+// import '../features/projects/styles/CreateProject.scss'
 import Input from '../features/projects/components/Input'
 import Button from '../components/Button'
+import { Controller, useForm } from 'react-hook-form';
+import { Card, TextField, Typography } from '@mui/material';
+import { Box, Container, Stack } from '@mui/system';
 
 export default function CreateProject() {
+  const { register, handleSubmit, control } = useForm();
   return (
-    <div className='form-container'>
-      <form method='post'>
-        <h2 style={{
-          color: "white"
-        }}>Create new project</h2>
-        <Input label='Project Title' />
-        <Input label='Project Decsription' />
-        <Input label='My Role' />
-        <Button >Create Project</Button>
-      </form>
-    </div>
+
+    // <Box component='form' sx={{
+    //   backgroundColor: 'white',
+    //   p: 1,
+    //   display: 'flex',
+    //   flexDirection: 'column',
+    //   alignItems: 'center',
+    //   gap: 1,
+    // }}>
+
+
+    // </Box>
+    <Box width="100vw" display='flex' justifyContent='center' pt={5}>
+      <Stack borderRadius={3} maxWidth='sm' direction='column' bgcolor='white' alignItems='center' gap={2} p={3} >
+        <Typography variant='h3' color='black'>Create Project</Typography>
+        <Box>
+          <TextField
+            sx={{
+              minWidth: '300px'
+            }}
+            // error
+            id=""
+            label="Title"
+          // defaultValue=""
+          />
+        </Box>
+        <Box>
+          <TextField
+            sx={{
+              minWidth: '300px'
+            }}
+            // error
+            id=""
+            label="My Role"
+          // defaultValue=""
+          />
+        </Box>
+        <Box>
+          <TextField
+            // error
+            id=""
+            label="Description"
+            multiline
+            minRows={4}
+            sx={{
+              minWidth: '300px'
+            }}
+          // defaultValue=""
+          />
+        </Box>
+        <Box>
+          <Button variant="contained">Create Project</Button>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
